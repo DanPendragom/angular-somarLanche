d291bb5b931a7a1f2db80d1d8ac9de31f70bebb9
DanPendragom <carlos.coutinho5@etec.sp.gov.br>
2019-10-07 15:25:09 -0300

    Adicionando componente soma

--- src/app/somar/somar.module.ts
+++ src/app/somar/somar.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SomaComponent } from './components/soma/soma.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SomaComponent],
   imports: [
     CommonModule
   ]
