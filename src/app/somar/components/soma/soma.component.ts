153b09e934545a6f5b04512b74e9c936df869fcf
Daniel Pendragon <carlos.coutinho5@etec.sp.gov.br>
2019-10-08 09:53:24 -0300

    Criando serviço de Quantidades e integrando na view

--- src/app/somar/components/soma/soma.component.ts
+++ src/app/somar/components/soma/soma.component.ts
@@ -11,17 +11,43 @@ export class SomaComponent implements OnInit {
   constructor(private soma: SomaService) { }
 
   private  valor: number;
+  private qtdLanches: number;
+  private qtdBebidas: number;
+  private qtdDoces: number;
 
   ngOnInit() {
     this.valor = 0;
+    this.qtdLanches = 0;
+    this.qtdBebidas = 0;
+    this.qtdDoces = 0;
   }
 
   calculeComp(valorRecebido: number):number{
     return this.valor = this.soma.calcule(valorRecebido,this.valor)
   }
 
+  quantidadeLanches(valorAtual: number): number{
+    return this.qtdLanches = this.qtdLanches + 1;
+  }
+  quantidadeBebidas(valorAtual: number): number{
+    return this.qtdBebidas = this.qtdBebidas + 1;
+  }
+  quantidadeDoces(valorAtual: number): number{
+    return this.qtdDoces = this.qtdDoces + 1;
+  }
+
   get getValor(){
     return this.valor;
+  }
+
+  get getQtdLanches(){
+    return this.qtdLanches;
+  }
+  get getQtdBebidas(){
+    return this.qtdBebidas;
+  }
+  get getQtdDoces(){
+    return this.qtdDoces;
   }
 
 }
